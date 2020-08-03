f7c8cd5c89be9468a46c1cf583adb5330cd8c72d
“[mohammodh.irfan]” <[mohammodh.irfan@innoeye.com]>
2020-08-04 01:09:57 +0530

    fix: environment fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,25 +13,11 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatMenuModule} from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon'
-
-
-
-
+import { MatIconModule } from '@angular/material/icon';
 import { AppCrudOperationComponent } from './app-crud-operation/app-crud-operation.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UploadComponent } from './upload/upload.component';
-
-const config = {
-  apiKey: "AIzaSyD9PL1JghaUxT36uFIRHnfXygZmfqd-Xl4",
-    authDomain: "project-sms-for-sms.firebaseapp.com",
-    databaseURL: "https://project-sms-for-sms.firebaseio.com",
-    projectId: "project-sms-for-sms",
-    storageBucket: "project-sms-for-sms.appspot.com",
-    messagingSenderId: "262893239167",
-    appId: "1:262893239167:web:74d265a717018a301c384a",
-    measurementId: "G-6VYPYXR14S"
-};
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -42,7 +28,7 @@ const config = {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(config),
+    AngularFireModule.initializeApp(environment.firebase),
     BrowserAnimationsModule,
     MatInputModule,
     FormsModule,
